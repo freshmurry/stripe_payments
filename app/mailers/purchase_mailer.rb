45abade4c1872cec0fc4b53b7a6766bea705859d
Lawrence Murry <lawrencemurry@yahoo.com>
2016-05-30 11:42:42 -0500

    Adding emailing of a purchase receipt

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "One Month Ecommerce <lawrencemurry@yahoo.com>"
+	default from: "One Month Ecommerce <ecommerce@onemonth.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
