9ab07951ff2632ba8308725b1c10696b52b86fd8
Lawrence Murry <lawrencemurry@yahoo.com>
2016-05-29 21:04:56 -0500

    Adding email of a purchase receipt

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,13 @@
 class Purchase < ActiveRecord::Base
+	
+	after_create :email_purchaser
+
+	private
+
+	def email_purchaser
+	   PurchaseMailer.purchase_receipt(self).deliver
+	end
+
 	def to_param
 		uuid
 	end	
--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base/smtp_settings = {
-	:address = 'smtp_sendgrid.net',
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp_sendgrid.net',
 	:port => '587',
 	:authentications => :plain,
 	:user_namer => ENV['SENDGRID_USERNAME'],
