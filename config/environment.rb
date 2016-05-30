e9800463e79ee99890654aba01fa6de424e5a161
Lawrence Murry <lawrencemurry@yahoo.com>
2016-05-29 19:58:56 -0500

    Implemented gemfiles for handling emailing in both development and production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base/smtp_settings = {
+	:address = 'smtp_sendgrid.net',
+	:port => '587',
+	:authentications => :plain,
+	:user_namer => ENV['SENDGRID_USERNAME'],
+	:password => ENV['SENDGRID_PASSWORD'],
+	:domain => 'heroku.com',
+	:enable_startstls_auto => true
+}
