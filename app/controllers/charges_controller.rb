20a1881a359fc50646a6bf57f6e4e43f9b6f2d20
Lawrence Murry <lawrencemurry@yahoo.com>
2016-06-03 22:30:17 -0500

    Reworked views to use product information from the database

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,27 +1,26 @@
 class ChargesController < ApplicationController
 	
 	def create
-		 #product = Product.find_by_sku("GROHACK2")
+		product = Product.find_by_sku("GROHACK1")
 
 	  customer = Stripe::Customer.create(
 	   :email => params[:stripeEmail],
 	   :card  => params[:stripeToken]
-		 #:plan => "GROHACK1"
   	)
 
 		charge = Stripe::Charge.create(
 		 :customer    => customer.id,
 		 :amount      => params[:amount],
-		 :description => 'Growth Hacking Crash Course',
+		 :description => product.full_description,
 		 :currency    => 'usd'
 		 )
 
     purchase = Purchase.create(email: params[:stripeEmail], 
-      card: params[:stripeToken], amount: params[:amount],
-      description: charge.description, currency: charge.currency,
-      customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
+     card: params[:stripeToken], amount: product.price_in_cents,
+     description: charge.description, currency: charge.currency,
+     customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
-    redirect_to purchase 
+    redirect_to purchase 	
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
